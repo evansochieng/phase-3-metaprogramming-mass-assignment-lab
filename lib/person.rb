3d533d0aabfdd4ef3710aa35cd0a6ce0b47c6c54
Evans Ochieng <evansochieng121@gmail.com>
2022-10-25 13:40:35 +0300

    add comments to code

--- lib/person.rb
+++ lib/person.rb
@@ -5,8 +5,8 @@ class Person
   def initialize(attributes)
     #iterate over the hash of attributes passed
     attributes.each do |key, value|
-      self.class.attr_accessor(key) #add getter and setter method to the key of every attribute
-      self.send("#{key}=", value) #
+      self.class.attr_accessor(key) #dynamically add getter and setter method to the key of every attribute
+      self.send("#{key}=", value) #abstract away the attribute names by using #send method
     end
   end
 end
