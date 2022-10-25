fa4b986e0335e1743162d0b7c352b6c5170e4df7
Evans Ochieng <evansochieng121@gmail.com>
2022-10-25 12:50:14 +0300

    practice using keyword arguments/mass assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,12 @@
+# use keywords arguments to define initialize method
+# use mass assignment in initialize method
 class Person
   # your code here
+  def initialize(attributes)
+    #iterate over the hash of attributes passed
+    attributes.each do |key, value|
+      self.class.attr_accessor(key) #add getter and setter method to the key of every attribute
+      self.send("#{key}=", value) #
+    end
+  end
 end
